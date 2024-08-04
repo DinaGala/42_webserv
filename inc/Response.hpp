cba91c8d2cd28efee2911928757bfd7a3e9c8b15
Núria Ferrón Fèrriz <nuriaf207@gmail.com>
2024-08-04 15:22:16 +0200

    cleaning + added defines

--- inc/Response.hpp
+++ inc/Response.hpp
@@ -7,8 +7,14 @@
 # define MAXCONNECT 10
 #endif
 
-# define AUTOINDEX(content) std::string("<html><head><title>AUTOINDEX</title><h1 style=\"text-align:center;font-size:200%;\">Index of ") + content + std::string("</h1></head><body style=\"font-size:150%;margin:50px;\">")
-# define AUTOINDEX_FILES(href, name) std::string("<p><a href= ") + href + std::string(">") + name + std::string("</a></p>\n")
+# define AUTOINDEX(content) \
+	std::string("<html><head><title>AUTOINDEX</title><h1 style=\"text-align:center;font-size:200%;\">Index of ") \
+			+ content + std::string("</h1></head><body style=\"font-size:150%;margin:50px;\">")
+# define AUTOINDEX_FILES(href, name) \
+	std::string("<p><a href= ") + href + std::string(">") + name + std::string("</a></p>\n")
+# define SEV_ERR "HTTP/1.1 505 Severe Internal Server Error\r\n" \
+				"Content-Type: text/plain\r\nContent-Length: 47\r\n\r\n" \
+				"Severe Internal Error.\nPlease, try again later\n"
 
 #include <map>
 #include <string>
@@ -28,50 +34,42 @@ class	Response
 	public:
 		Response();
 		Response(const Response &r);
-		//Response(Request &req);
+		Response	&operator=(const Response &r);
 		~Response();
 
-		Response	&operator=(const Response &r);
-		//SET VARS
-		void		setBody(const std::string &msg);
-		void		setCgiPath(const std::string &cgi);
-		void		setCode(const int &code);
-		void		setReq(const Request *rqt);
+		std::string	&makeResponse(const Request *req);
+		void		cleanResponse(); // TODO for multiplexing
+		/************** GETTERS *******************/
 		const std::string& 		getResponse() const;
-		int				getCode() const;
-		//WRITE RESPONSE
+		int						getCode() const;
+
+	private:
+		std::map<int, std::pair<std::string, std::string> >	_status;
+		std::vector<std::string>	_cgiargs;
+		std::string		_body;
+		std::string		_response;
+		unsigned int	_code;
+		const Request	*_req;
+
+		/************ WRITE RESPONSE **************/
 		std::string	putStatusLine(int code);
 		void		putGeneralHeaders(void);
 		bool		putPostHeaders(const std::string &file);
 		int			fileToBody(const std::string &path);
-		//SEND RESPONSE
-		void		cleanResponse(); // TODO for multiplexing
-
-		std::string	&makeResponse(const Request *req);
 		void		sendError(int code);
-		std::string	ft_strnstr(const std::string &h, const std::string &n);
-
-	private:
-		//static std::map<int, std::pair<std::string, std::string> >	_status;
-		//static std::map<int, std::pair<std::string, std::string> >	_initStatus();
-		std::map<int, std::pair<std::string, std::string> >	_status;
-		std::string		_body;
-		std::string		_reqbody;
-		std::string		_response;
-		std::vector<std::string>	_cgiargs;
-		unsigned int	_code;
-		const Request	*_req;
-
-		void		_parseCgiResponse(void);
+		/************ HANDLE METHODS **************/
 		void		_handleGet(void);
 		void		_handlePost(void);
 		void		_handleDelete(void);
-		std::string	_parseUrl(const std::string &url);
+		/***************** CGI *******************/
 		std::vector<std::string> _findCgiArgs(const std::string &path);
+		void		_parseCgiResponse(void);
+		/*********** METHODS' UTILS **************/
 		bool		_createFile(void);
 		bool		_isAccepted(std::string str);
-		void		_makeAutoIndex(void);
 		int			_isDir(const std::string &path) const ;
+		/************ SPECIAL REQUESTS **********/
+		void		_makeAutoIndex(void);
 		void		_handleFavIcon();
 };
 
