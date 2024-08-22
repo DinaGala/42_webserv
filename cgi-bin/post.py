f7ac32be1e658fcbd6a3a8e47cd1365f542b6471
Núria Ferrón Fèrriz <nuriaf207@gmail.com>
2024-08-22 14:35:05 +0200

    cleaning main

--- cgi-bin/post.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = 'http://localhost:8081/upload'
-myobj = {'somekey': 'somevalue'}
-
-x = requests.post(url, json = myobj)
-
-print(x.text)
-
--- cgi-bin/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Define the URL for the POST request
-url="http://localhost:8081/cgi"
-
-# Define the filename and data to write
-url="localhost:8081/cgi"
-
-curl -X POST -F 'file=@"src/Utils.cpp";filename="uploaded_files"' $url
